424f7341b46b47eb794e41b0d9c65ef201af3eac
Barry Grussling <barry@grussling.com>
2013-07-17 10:35:26 -0700

    Playing with my own theme based off prose

--- lib/git.zsh
+++ lib/git.zsh
@@ -2,9 +2,13 @@
 function git_prompt_info() {
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  echo "%{$fg[magenta]%}$(my_current_repository)%{$reset_color%}$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
+my_current_repository() {
+  my_repo=$(basename $(git rev-parse --show-toplevel 2> /dev/null)) || return
+  echo ${my_repo}
+}
 
 # Checks if working tree is dirty
 parse_git_dirty() {
